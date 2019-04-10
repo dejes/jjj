9094b6fcf7161742eec72db2bbf62b3d352078fd
hsumax0216 <b26262235@gmail.com>
2019-04-10 14:04:02 +0800

    AAA

--- jjj/ViewController.swift
+++ jjj/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var price: UILabel!
+    @IBOutlet var enter: UIButton!
+    @IBOutlet var pic: UIImageView!
+    @IBOutlet var rnd: UISwitch!
+    @IBOutlet var brend: UISegmentedControl!
+    @IBOutlet var priceslider: UISlider!
+    @IBOutlet var tf: UITextField!
+    var r=Int.random(in:0...1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func sl(_ sender: Any) {
+        price.text=String(Int(priceslider.value))
+    }
+    
+    @IBAction func ent(_ sender: Any) {
+        if let input=tf.text,let _num=Int(input){
+            price.text = String(_num)
+            
+        }
+        
+        view.endEditing(true)
+       
+    }
+    
+    @IBAction func rndswitch(_ sender: UISwitch) {
+        
+        if (sender.isOn) {
+            
+        }
+    }
+    
+    
 }
 
